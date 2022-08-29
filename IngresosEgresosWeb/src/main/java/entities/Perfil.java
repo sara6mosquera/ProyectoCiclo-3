7cbfeb3c701e8a519a71ac68e3b356e5fa2ae129
Kellyingeniera25 <111308986+Kellyingeniera25@users.noreply.github.com>
2022-08-28 21:33:08 -0500

    Add files via upload
    commit to main of "Perfil.java" file

---
+++ IngresosEgresosWeb/src/main/java/entities/Perfil.java
@@ -0,0 +1,50 @@
+package entities;
+import java.util.Date;
+
+public class Perfil{
+ //Atributos
+    long id;
+    String image;
+    String phone;
+
+    Date createdAt;
+    Date updatedAt;
+
+    //Basados en Clases
+    Empleado user;
+    
+    //Constructor
+    public Perfil()
+    {
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+    }
+
+    //Getters y Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id=  id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image=  image;
+    }
+
+	public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone=  phone;
+    }
+
+    
+
+}
