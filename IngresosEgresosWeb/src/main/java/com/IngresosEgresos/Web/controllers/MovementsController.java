1ba56084d6e77eb00f2de320bb54970dc8895792
naki29 <48079374+naki29@users.noreply.github.com>
2022-09-06 11:00:32 -0500

    Add files via upload

---
+++ IngresosEgresosWeb/src/main/java/com/IngresosEgresos/Web/controllers/MovementsController.java
@@ -0,0 +1,31 @@
+package com.IngresosEgresos.Web.controllers;
+import com.IngresosEgresos.Web.services.EnterpriseServices;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class MovementsController {
+	///enterprises/[id]/movements
+   @GetMapping("/enterprises/{id}/movements")
+    public String consultarMovimientos(@PathVariable long id)
+    {
+        return "200";
+    }
+ 
+    @PostMapping("/enterprises/{id}/movements")
+    public String consultarMovimientos1(@PathVariable long id)
+    {
+        return "200";
+    }
+
+    @PatchMapping("/enterprises/{id}/movements")
+    public String consultarMovimientos2(@PathVariable long id)
+    {
+        return "200";
+    }
+    @DeleteMapping("/enterprises/{id}/movements")
+    public String borrarMovimientos(@PathVariable long id)
+    {
+        return "200";
+    }
+}
