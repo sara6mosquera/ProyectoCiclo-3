9f2477094dbf96b6fe3de94a668533a6c61f3d0e
sara.dmosquera@hotmail.com <sara.dmoquera@hotmail.com>
2022-09-07 09:51:51 -0500

    (sprint 3), clases creadas

---
+++ IngresosEgresosWeb/src/main/java/com/IngresosEgresos/Web/services/UserServices.java
@@ -0,0 +1,33 @@
+package com.IngresosEgresos.Web.services;
+
+public class UserServices {
+    //El sistema permite consultar todas los usuarios
+    //El sistema permite consultar un solo usuario
+    //El sistema permite crear un usuario
+    //El sistema permite editar un usuario
+    //El sistema permite eliminar un usuario
+
+    public String consultarUsuarios()
+    {
+        return "hola";
+    }
+    public String consultarUnUsuario(long id)
+    {
+        return "mundo";
+    }
+    public String crearUsuario()
+    {
+        return "mundo";
+    }
+
+    public String editarUnUsuario(long id)
+    {
+        return "mundo";
+    }
+
+    public String eliminarUnUsuario(long id)
+    {
+        return "mundo";
+    }
+
+}
